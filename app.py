94320d87c477bb8002e712cb04033e58736c6378
Eric Brose <ericbrose@gmail.com>
2022-10-26 13:14:23 -0700

    make changes to use dictionaries instead of lists

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
-# stores = [
-#     {
-#         "name": "My Store",
-#         "items": [
-#             {
-#             "name": "Chair",
-#             "price": 15.99
-#             }
-#         ]
-#     }
-# ]
 
-stores = {}
-item = {
-    1: {
-        "name":"Chair",
-        "price":17.99
-    },
-    2: {
-        "name":"Table",
-        "price":180.50
-    }
-}
 
 
 # flask route
@@ -32,24 +12,27 @@ item = {
 # get em all
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    # have to convert to a list here
+    return {"stores": list(stores.values())}
 
 #data for 1 store
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
         return {"message":"Store not found"}, 404
 
-# items in a particular store
-@app.get("/store/<string:name>/item")
-def get_items_in_store(name):
-    for store in stores:
-        print(store["name"])
-        if store["name"] == name:
-            return {"items": store["items"]}
-        return {"message":"Store not found"}, 404
+
+
+
+# retrieve a single item
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message":"Item not found"}, 404
 
         
 
@@ -57,21 +40,34 @@ def get_items_in_store(name):
 
 
 @app.post("/store")
-def create_stores():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+def create_store():
+    store_data = request.get_json()
+    # make a uuid or autoincrementer
+    store_id = uuid.uuid4().hex
+
+    # passing keyword args to constructor
+    store = {**store_data, "id":store_id}
+    stores[store_id] = store
+
+    return store, 201
 
 
 
-@app.post("/store/<string:name>/item")
+@app.post("/item")
 def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item
-    return {"message": "Store not found"}, 404
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    # assume we have received the expected payload
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id":item_id}
+    items[item_id] = item
+    return item, 201
+
+
+@app.get("/item")
+def get_all_items():
+    # have to convert to a list here
+    return {"items": list(items.values())}
 
